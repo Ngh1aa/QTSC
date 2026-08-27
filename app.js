64a6142672ffe8b596f743cf5e2056faac575b41
Nghĩaaaa <93433772+Haign12@users.noreply.github.com>
2026-08-27 20:18:21 +0700

    chore: remove obsolete homepage loader

--- app.js
+++
@@ -1,7 +0,0 @@
-/** Homepage loader — feature code lives in assets/js/pages/home.js. */
-(() => {
-  const script = document.createElement('script');
-  script.src = 'assets/js/pages/home.js';
-  script.defer = true;
-  document.head.append(script);
-})();
